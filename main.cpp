e468e590540e08708bd8224de1d3c9a6c64d81dc
Sven Cerk <sven.cerk@gmail.com>
2017-05-02 23:00:40 +0200

    refactoring and non-general position

--- main.cpp
+++ main.cpp
@@ -5,41 +5,72 @@
 #include <algorithm>
 #include <cmath>
 
+#include <iostream>
 
 using polygon_t = std::vector<std::pair<double,double>>;
 
-std::pair<double,double> split_area(const polygon_t & polygon, double split_x) {
+double left_area(const polygon_t & polygon, double split_x) {
     auto n = polygon.size();
-
-    double left_area = 0.0;
-    double right_area = 0.0;
-
+    double area = 0.0;
     for(polygon_t::size_type i=0; i<n; ++i) {
-        auto x0 = polygon[i].first - split_x;
+        auto x0 = polygon[i].first;
         auto y0 = polygon[i].second;
-        auto x1 = polygon[(i+1)%n].first - split_x;
+        auto x1 = polygon[(i+1)%n].first;
         auto y1 = polygon[(i+1)%n].second;
-
-        if(x0 <= 0 && x1 <= 0) {
-            left_area  += 0.5 * (x1 - x0) * (y1 + y0);
-        } else if(x0 >= 0 && x1 >= 0) {
-            right_area += 0.5 * (x1 - x0) * (y1 + y0);
-        } else if(x0 < 0) {
-            auto ax0 = std::abs(x0);
-            auto ax1 = std::abs(x1);
+        if(x0 <= split_x && x1 <= split_x) {
+            area  += 0.5 * (x1 - x0) * (y1 + y0);
+        } else if(x0 < split_x || x1 < split_x){
+            auto ax0 = std::abs(x0 - split_x);
+            auto ax1 = std::abs(x1 - split_x);
             auto dx = ax0 + ax1;
-            left_area  += 0.5 * (-x0) * (ax1/dx * y0 + ax0/dx * y1 + y0);
-            right_area += 0.5 * ( x1) * (y1 + ax1/dx * y0 + ax0/dx * y1);
-        } else if (x0 > 0) {
-            auto ax0 = std::abs(x0);
-            auto ax1 = std::abs(x1);
-            auto dx = ax0 + ax1;
-            left_area  += 0.5 * ( x1) * (y1 + ax1/dx * y0 + ax0/dx * y1);
-            right_area += 0.5 * (-x0) * (ax1/dx * y0 + ax0/dx * y1 + y0);
+            if(x0 < split_x) {
+                area += 0.5 * (split_x-x0) * (ax1/dx * y0 + ax0/dx * y1 + y0);
+            } else {
+                area += 0.5 * (x1-split_x) * (y1 + ax1/dx * y0 + ax0/dx * y1);
+            }
         }
     }
+    return area;
+}
 
-    return {left_area, right_area};
+double polygon_height_at_x(const polygon_t & polygon, double x, bool lean_left) {
+    auto n = polygon.size();
+    std::vector<double> intersections;
+    for(polygon_t::size_type i=1; i<=n; ++i) {
+        auto x1 = polygon[i-1].first - x;
+        auto y1 = polygon[i-1].second;
+        auto x2 = polygon[i%n].first - x;
+        auto y2 = polygon[i%n].second;
+        if( x1 * x2 <= 0
+             && (   ( lean_left && (x1 < 0 || x2 < 0))
+                 || (!lean_left && (x1 > 0 || x2 > 0)))) {
+            auto ax1 = std::abs(x1);
+            auto ax2 = std::abs(x2);
+            auto y = (ax2 * y1 + ax1 * y2) / (ax1 + ax2);
+            intersections.push_back(y);
+        }
+    }
+    std::sort(begin(intersections),end(intersections));
+    double height = 0.0;
+    for(std::size_t i=0; i<intersections.size(); i+=2) {
+        height += intersections[i+1] - intersections[i];
+    }
+    return height;
+}
+
+double solve_quad_eqn(double a, double b, double c, double sx, double ex) {
+    if(a != 0) {
+        auto sqrtD = std::sqrt(b*b - 4*a*c);
+        auto x1 = (-b + sqrtD) / (2*a);
+        auto x2 = (-b - sqrtD) / (2*a);
+        if(sx <= x1 && x1 <= ex) {
+            return x1;
+        } else {
+            return x2;
+        }
+    } else {
+        return -c / b;
+    }
 }
 
 double bisecting_line(const polygon_t & polygon) {
@@ -49,95 +80,49 @@ double bisecting_line(const polygon_t & polygon) {
     std::sort(begin(idxs),end(idxs), [&polygon](int a, int b) {
         return polygon[a] < polygon[b];
     });
-
+    auto area = left_area(polygon, std::numeric_limits<double>::infinity());
     // bisection
     polygon_t::size_type left  = 0;
+    double lla = 0.0;
     polygon_t::size_type right = n-1;
     while(right - left > 1) {
-        polygon_t::size_type mid = (left + right) / 2;
+        polygon_t::size_type mid = left + (right - left) / 2;
         auto split_x = polygon[idxs[mid]].first;
-        double left_area, right_area;
-        std::tie(left_area,right_area) = split_area(polygon,split_x);
-        if(left_area < right_area) {
+        auto mla = left_area(polygon,split_x);
+        if(mla < area/2) {
             left = mid;
+            lla = mla;
         } else {
             right = mid;
         }
     }
     auto lx = polygon[idxs[left]].first;
     auto rx = polygon[idxs[right]].first;
-
-    double lla, lra, rla, rra; // TODO: can use results from the while loop
-    std::tie(lla,lra) = split_area(polygon,lx);
-    std::tie(rla,rra) = split_area(polygon,rx);
-    double area = lla + lra;
-
     // compute final line
-    std::vector<double> left_intersections;
-    for(polygon_t::size_type i=1; i<=n; ++i) {
-        auto x1 = polygon[i-1].first - lx;
-        auto y1 = polygon[i-1].second;
-        auto x2 = polygon[i%n].first - lx;
-        auto y2 = polygon[i%n].second;
-        if( x1 * x2 <= 0 && (x1 < 0 || x2 < 0) ) {
-            auto ax1 = std::abs(x1);
-            auto ax2 = std::abs(x2);
-            auto y = (ax2 * y1 + ax1 * y2) / (ax1 + ax2);
-            left_intersections.push_back(y);
-        }
-    }
-    std::sort(begin(left_intersections),end(left_intersections));
-
-    std::vector<double> right_intersections;
-    for(polygon_t::size_type i=1; i<=n; ++i) {
-        auto x1 = polygon[i-1].first - rx;
-        auto y1 = polygon[i-1].second;
-        auto x2 = polygon[i%n].first - rx;
-        auto y2 = polygon[i%n].second;
-        if( x1 * x2 <= 0 && (x1 < 0 || x2 < 0) ) {
-            auto ax1 = std::abs(x1);
-            auto ax2 = std::abs(x2);
-            auto y = (ax2 * y1 + ax1 * y2) / (ax1 + ax2);
-            right_intersections.push_back(y);
-        }
-    }
-    std::sort(begin(right_intersections),end(right_intersections));
-
-    double left_height = 0.0;
-    for(std::size_t i=0; i<left_intersections.size(); i+=2) {
-        left_height += left_intersections[i+1] - left_intersections[i];
-    }
-    double right_height = 0.0;
-    for(std::size_t i=0; i<right_intersections.size(); i+=2) {
-        right_height += right_intersections[i+1] - right_intersections[i];
-    }
-
-    // solve the quadratic equation
+    double left_height  = polygon_height_at_x(polygon, lx, false);
+    double right_height = polygon_height_at_x(polygon, rx, true);
     auto dx = rx - lx;
     auto a = (right_height - left_height) / (2 * dx);
-    auto b = left_height / dx;
+    auto b = left_height;
     auto c = lla - area/2;
-    if(a == 0) {
-        return -c / left_height;
-    }
-    auto D = b*b - 4*a*c;
-    auto x1 = (-b + sqrt(D)) / (2*a);
-    auto x2 = (-b - sqrt(D)) / (2*a);
-    if(0 <= x1 && x1 <= dx) {
-        return lx+x1;
-    } else {
-        return lx+x2;
-    }
+    return lx + solve_quad_eqn(a,b,c,0,dx);
 }
 
-#include <iostream>
 using namespace std;
 
 int main() {
     polygon_t polygon{{0,7},{7,8},{8,0},{3,1},{6,2},{2,3},{5,4},{1,5},{4,6}};
 
-    auto x = bisecting_line(polygon);
+    double x, la;
+
+    x = bisecting_line(polygon);
     cout << x << endl;
-    auto areas = split_area(polygon, x);
-    cout << areas.first << " " << areas.second << endl;
+    la = left_area(polygon, x);
+    cout << la << endl;
+
+    polygon_t p1{{0,1},{1,1},{1,0},{0,0}};
+    x = bisecting_line(p1);
+    cout << x << endl;
+    la = left_area(p1, x);
+    cout << la << endl;
 }
